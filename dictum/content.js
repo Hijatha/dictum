d4ea92e061994cad12446f70150dc71099bc07b9
Jake <jake.e.schuurmans@gmail.com>
2018-04-24 09:40:36 -0400

    Add files via upload
    Finished basic CSS for comments, will need more work, but functional now.

---
+++ dictum/content.js
@@ -0,0 +1,44 @@
+// jQuery(document).ready(function(){
+	// $('#body-text').after(genComment(comments[0]));
+function genBox()
+{
+	document.write(genComment(comments[0]));
+}
+// });
+
+function genComment(commentChain)
+{
+	var html = "";
+	html = createBox(commentChain, 'comment', html);
+
+	if (!jQuery.isEmptyObject(commentChain.reply))
+	{
+		html = genReplies(commentChain.reply, html);
+	}
+	html = html + '</div>';
+
+	return html;
+}
+
+function genReplies(commentChain, html)
+{
+	html = createBox(commentChain, 'reply', html);
+
+	if (!jQuery.isEmptyObject(commentChain.reply))
+	{
+		html = genReplies(commentChain.reply, html);
+	}
+	html = html + '</div>';
+
+	return html;
+}
+
+function createBox(commentChain, type, html)
+{
+	html = html + '<div class="' + type + '"><b>' +
+	commentChain.handle + '</b><i><small><b> &#8226 </b>' +
+	commentChain.points + ' points<b> &#183 </b>' +
+	commentChain.time + ' hours ago</small></i><br>' +
+	commentChain.comment + '<br>';
+	return html;
+}
