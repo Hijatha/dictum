202d12de5cb7aba1cc0651ffa60d6807234b6f1c
Jake <jake.e.schuurmans@gmail.com>
2018-04-24 13:53:04 -0400

    Addition of nonfunctional collapsing bars.
    Removed borders on replies in addition to the collapsing bars, since they had the same aesthetics of the border.

--- content.js
+++ content.js
@@ -28,14 +28,14 @@ function genReplies(commentChain, html)
 	{
 		html = genReplies(commentChain.reply, html);
 	}
-	html = html + '</div>';
+	html = html + '</div></div>';
 
 	return html;
 }
 
 function createBox(commentChain, type, html)
 {
-	html = html + '<div class="' + type + '"><b>' +
+	html = html + '<div class="' + type + '"><p class="collapse"><a href="javascript:void(0);">[&#8211]</a></p><div><b>' +
 	commentChain.handle + '</b><i><small><b> &#8226 </b>' +
 	commentChain.points + ' points<b> &#183 </b>' +
 	commentChain.time + ' hours ago</small></i><br>' +
